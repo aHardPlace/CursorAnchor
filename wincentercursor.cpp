defc59276e5fd5693944915884641ff18d0ef90e
aHardPlace <aHardPlace@users.noreply.github.com>
2015-12-24 21:30:07 -0800

    Optimizations

--- wincentercursor.cpp
+++ wincentercursor.cpp
@@ -1,15 +1,15 @@
 #include <windows.h>
 
 int main() {
+	const int centerScreenX = GetSystemMetrics(SM_CXSCREEN) / 2;
+	const int centerScreenY = GetSystemMetrics(SM_CYSCREEN) / 2;
 	const int highBit = (1 << 16);
-	SHORT curKeyState;
 	while (true) {
-		curKeyState = GetAsyncKeyState(VK_F3);
-		if (highBit & curKeyState) {
-			SetCursorPos(GetSystemMetrics(SM_CXSCREEN) / 2, GetSystemMetrics(SM_CYSCREEN) / 2);
+		if (highBit & GetAsyncKeyState(VK_F3)) {
+			SetCursorPos(centerScreenX, centerScreenY);
 		}
 
-		Sleep(40);
+		Sleep(40); // sensibly arbitrary milliseconds
 	}
 
 	return 0;
